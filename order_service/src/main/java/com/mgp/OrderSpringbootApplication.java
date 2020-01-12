76972ab4896003ccb090d4bb469a15e1d94e7795
maoguangping <991335931@qq.com>
2020-01-12 15:23:10 +0800

    order+smt

---
+++ order_service/src/main/java/com/mgp/OrderSpringbootApplication.java
@@ -0,0 +1,16 @@
+package com.mgp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSpringbootApplication.class);
+    }
+
+}
